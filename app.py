691c4116844861ee0bdae6beead303dc018a214a
test user <Test@agpl.net>
2026-07-03 12:28:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,69 @@
+#text is witeen in  " " or ' '
+print("hio"); 
+print("hii");
+print("Hello World!");
+print("Hello World!");
+print("Hello World!");
+print("Hello World!");
+print("This will work!");
+# for new next line to appear in same line use end="" so that it prints in same line
+print("This will work!", end=" ");
+print("This will work!");
+print("This will work!");
+# numbers r written withpuit quotes
+print(3);
+print(358);
+# for adding   of number normal addition is used
+print(3+4);
+print(3+3+7);
+# BY ASSIGNING A VARIABLE 
+#(variable r containers for storinfg data values)
+X=5;
+Y=10;
+print(X+Y);
+
+#This is a single line comment
+#FOR ADDING TEXT AND NUYMBERS TOGETHER, it can be done by serrprating with comma
+print("I am ", 23,"years old.");
+# Write a single-line comment
+
+# Comment out this line so it does not run:
+
+
+# This is a multiline comment
+"""This is a multiline comment"""
+#casting  is a proces  of specifying the data type of the variable 
+x=str(3);
+y=int(3);
+z=float(3);
+#to print
+print(X);
+print(Y);
+print(z);
+# strings can be declare din a single or double quotes
+x="hi";
+y='hi';
+print(x+y)   # both r same 
+
+
+a=3
+A=5
+#a will not overwrite A because python is case sensitive
+
+# variabe must start with only letters, or underscore there cnnot be space or special character between them.
+# IT CAN CONTAIN alphabets, numbers, underscores int it
+# in it we can asign multiple vales to multiple vvariables or asign same vale to multiple variable 
+
+# example:assign multiple values to multiple variables
+x,y,z="orange","banana","cherry"
+print(x,y,z)
+# assign same value to ,ultiple variables
+x=y=z="orange"
+print(x,y,z)
+
+
+
+
+
+
+
